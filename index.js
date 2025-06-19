fc5699f433ab78c934c5550530116bdd6ff75531
Rebecca Kibisu <coursecpd@gmail.com>
2025-06-19 20:01:44 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendCat(kitty) {
+  cats.push(kitty); // Adds to the end
+ }
+
+ function destructivelyPrependCat(Tim) {
+  cats.unshift(Tim); // Adds to the beginning
+ }
+ function destructivelyRemoveLastCat() {
+  cats.pop(); 
+ }
+ function destructivelyRemoveFirstCat() {
+   cats.shift()
+ }
+ function appendCat(kelly) {
+    return [...cats, kelly]
+ }
+ function prependCat(Jey) {
+  return [Jey, ...cats];
+ }
+ function removeLastCat() {
+  return cats.slice(0, -1);
+ }
+
+ function removeFirstCat() {
+  return cats.slice(1);
+ }
